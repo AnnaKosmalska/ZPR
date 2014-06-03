2f9408241cab893ce5bd2ec10619d4177d24ea2b
Ecthor <thorofwar@gmail.com>
2014-06-04 01:56:59 +0200

    Dodane komentarze.

--- server/new.py
+++ server/new.py
@@ -2,11 +2,11 @@
 
 #Serwer
 
-"""@package Python
-Modul Pythona sluzacy do komunikacji miedzy klientem a serwerem.
+##@package Python
+#Modul Pythona sluzacy do komunikacji miedzy klientem a serwerem.
+#
+#Modul ten dziala przy serwerze FastCGI. Za pomoca formatu JSON porozumiewa sie on z klientem i przetwarza jego zadania dla modelu gry. Po otrzymaniu odpowiedzi od modelu zwraca ja, odpowiednio sformatowana, do klienta.
 
-Modul ten dziala przy serwerze FastCGI. Za pomoca formatu JSON porozumiewa sie on z klientem i przetwarza jego zadania dla modelu gry. Po otrzymaniu odpowiedzi od modelu zwraca ja, odpowiednio sformatowana, do klienta.
-"""
 
 import os
 import sys
@@ -15,13 +15,12 @@ import os
 import model
 import random
 
+##Funkcja UpDate
+#
+#Jest uzywana przy przekazywaniu informacji dla wolania o aktualny stan gry.
+#@param co nazwa wolanej funkcji
+#@param auser przekazuje nazwe uzytkownika
 def UpDate(co, auser=0):
-"""Funkcja UpDate
-
-Jest uzywana przy przekazywaniu informacji dla wolania o aktualny stan gry.
-@param co nazwa wolanej funkcji
-@param auser przekazuje nazwe uzytkownika
-    """
 
 	if co == "GetScore":
 		return model.getScore(auser)
@@ -30,14 +29,12 @@ Jest uzywana przy przekazywaniu informacji dla wolania o aktualny stan gry.
 	if co == "getCurrentPlayer":
 		return model.getCurrentPlayer()
 
-
+##Funkcja App
+#
+#Glowna funckcja serwera. Przekazuje przychodzace od klienta zadania i wysyla zapytania do modelu. Komunikacja zostala zrealizowana za pomoca formatu JSON.
+#@param environ slownik WSGI
+#@param start_response callback do przesylania informacji
 def App(environ, start_response):
-    """Funkcja App
-    	
-    Glowna funckcja serwera. Przekazuje przychodzace od klienta zadania i wysyla zapytania do modelu. Komunikacja zostala zrealizowana za pomoca formatu JSON.
-    @param environ slownik WSGI
-    @param start_response callback do przesylania informacji
-    """
 	try:
 		packet_leng = int(environ.get('CONTENT_LENGTH', 0))
 	except (ValueError):
@@ -46,28 +43,28 @@ def App(environ, start_response):
 
 #pobieramy caly pakiet
 	packet = environ['wsgi.input'].read(packet_leng)
+	##Odczyt pobranego od klienta pakietu do orders
 	orders = json.loads(packet)
-	""" Odczyt pobranego od klienta pakietu do orders """
 
 #dodac wybieranie z jsona informacji, obsluga modelu
 	auser_id = orders["user"]
+	##Odbiera nazwe zadania od klienta, na podstawie ktorego nastepnie uruchamia odpowiednie metody modelu.
 	function = orders["function"]
-	""" Odbiera nazwe zadania od klienta, na podstawie ktorego nastepnie uruchamia odpowiednie metody modelu. """
 
 #funkcje
 	if function == "addPlayer":
 		data = "Nowy"
+		##Niezbedne kodowanie na utf8 przy przekazywaniu do C++.
 		data = data.encode('utf8')
-	""" Niezbedne kodowanie na utf8 przy przekazywaniu do C++ """
+
 		x = model.addPlayer(data)
 		x = dict(user=x)
-	""" Zwraca id uzytkownika """
 
 	if function == "choose":
+	##Wybor obrazka. Przekazuje id odebrane od klienta, rzutuje otrzymany od klienta string.
 		x = model.choose(auser_id, int(orders["row"]), int(orders["column"]))
-	""" Wybor obrazka. Przekazuje id odebrane od klienta, rzutuje otrzymany od klienta string """
+		##Zwraca id obrazka wybranego przez uzytkownika
 		x = dict(picture=x)
-	""" Zwraca id obrazka wybranego przez uzytkownika """
 
 
 	if function == "playerReady":
@@ -75,8 +72,9 @@ def App(environ, start_response):
 		x = dict(status=x)
 
 	if function == "initGame":
+		##Rozpoczecie gry. Rzutuje otrzymany od klienta string
 		x = model.initGame(int(orders["rows"]), int(orders["columns"]))
-	""" Rozpoczecie gry. Rzutuje otrzymany od klienta string """
+
 		x = dict(status=x)
 
 	if function == "endGame":
@@ -92,8 +90,8 @@ def App(environ, start_response):
 		x = dict(user_id=auser_id,State=x)
 
 	if function == "wattsUp":
+		##Zwraca strukture danych zawierajaca informacje na temat biezacej rozgrywki.
 		x = dict(score = UpDate("GetScore",  auser_id), status=UpDate("GetState"), player=UpDate("getCurrentPlayer"), testfirst=model.getFirstX(), first=[model.getFirstX(), model.getFirstY(), model.getFirstPicked()], second=[model.getSecondX(), model.getSecondY(), model.getSecondPicked()])
-	""" Zwraca strukturę danych zawierajaca informacje na temat biezacej rozgrywki. """
 
 #odpowiadanie
 	ret_packet = json.dumps(x)
