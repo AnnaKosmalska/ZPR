eecadc289068548587972bca570bab3975772579
Ecthor <thorofwar@gmail.com>
2014-06-04 01:27:39 +0200

    Dodane komentarze.

--- server/new.py
+++ server/new.py
@@ -2,6 +2,13 @@
 
 #Serwer
 
+"""@package Python
+Modul Pythona sluzacy do komunikacji miedzy klientem a serwerem.
+
+Modul ten dziala przy serwerze FastCGI. Za pomoca formatu JSON porozumiewa się on z klientem i przetwarza jego zadania dla modelu gry. 
+Po otrzymaniu odpowiedzi od modelu zwraca ja, odpowiednio sformatowana, do klienta.
+"""
+
 import os
 import sys
 import json
@@ -10,43 +17,57 @@ import model
 import random
 
 def UpDate(co, auser=0):
-	#int getScore(int gracz)
+    """Funkcja UpDate
+
+    Jest uzywana przy przekazywaniu informacji dla wolania o aktualny stan gry.
+    @param co nazwa wolanej funkcji
+    @param auser przekazuje nazwe uzytkownika
+    """
 	if co == "GetScore":
 		return model.getScore(auser)
-	#int getState() stan gry
 	if co == "GetState":
 		return model.getState()
-	#int getCurrentPlayer() kto teraz gra
 	if co == "getCurrentPlayer":
 		return model.getCurrentPlayer()
-	#int getFirst() pierwszy kartonik
 
 
 def App(environ, start_response):
-#sprawdzamy pakiet, pobieramy dlugosc
+    """Funkcja App
+    	
+    Glowna funckcja serwera. Przekazuje przychodzace od klienta zadania i wysyla zapytania do modelu. Komunikacja zostala zrealizowana za pomoca formatu JSON.
+    @param environ slownik WSGI
+    @param start_response callback do przesylania informacji
+    """
 	try:
 		packet_leng = int(environ.get('CONTENT_LENGTH', 0))
 	except (ValueError):
 		packet_leng = 0
+	""" Sprawdza pobierany pakiet """
 
 #pobieramy caly pakiet
 	packet = environ['wsgi.input'].read(packet_leng)
 	orders = json.loads(packet)
+	""" Odczyt pobranego od klienta pakietu do orders """
 
 #dodac wybieranie z jsona informacji, obsluga modelu
 	auser_id = orders["user"]
 	function = orders["function"]
+	""" Odbiera nazwe zadania od klienta, na podstawie ktorego nastepnie uruchamia odpowiednie metody modelu. """
 
 #funkcje
 	if function == "addPlayer":
 		data = "Nowy"
 		data = data.encode('utf8')
+	""" Niezbedne kodowanie na utf8 przy przekazywaniu do C++ """
 		x = model.addPlayer(data)
-		x = dict(user=1)
+		x = dict(user=x)
+	""" Zwraca id uzytkownika """
 
 	if function == "choose":
 		x = model.choose(auser_id, int(orders["row"]), int(orders["column"]))
+	""" Wybor obrazka. Przekazuje id odebrane od klienta, rzutuje otrzymany od klienta string """
 		x = dict(picture=x)
+	""" Zwraca id obrazka wybranego przez uzytkownika """
 
 
 	if function == "playerReady":
@@ -55,6 +76,7 @@ def App(environ, start_response):
 
 	if function == "initGame":
 		x = model.initGame(int(orders["rows"]), int(orders["columns"]))
+	""" Rozpoczecie gry. Rzutuje otrzymany od klienta string """
 		x = dict(status=x)
 
 	if function == "endGame":
@@ -71,6 +93,7 @@ def App(environ, start_response):
 
 	if function == "wattsUp":
 		x = dict(score = UpDate("GetScore",  auser_id), status=UpDate("GetState"), player=UpDate("getCurrentPlayer"), testfirst=model.getFirstX(), first=[model.getFirstX(), model.getFirstY(), model.getFirstPicked()], second=[model.getSecondX(), model.getSecondY(), model.getSecondPicked()])
+	""" Zwraca strukturę danych zawierajaca informacje na temat biezacej rozgrywki. """
 
 #odpowiadanie
 	ret_packet = json.dumps(x)
@@ -81,7 +104,7 @@ def App(environ, start_response):
 	return [ret_packet]
 
 
-#jesli jest to glowny modul, wlaczamy serwer
+#jesli jest to glowny modul, dolaczamy pythona do serwera.
 if __name__ == '__main__':
 	from flup.server.fcgi import WSGIServer
 	WSGIServer(App).run()
